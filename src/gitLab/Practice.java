f68a6acf9fafeb06b99d1a91287db6f7c4a2e99e
Caleb__C <caleb.cunningham@initechglobal.com>
2021-01-19 14:09:52 -0500

    Update all files

--- src/gitLab/Practice.java
+++ src/gitLab/Practice.java
@@ -2,9 +2,9 @@ package gitLab;
 
 public class Practice {
 	public String notString(String str) {
-		if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+		if (str.length() >= 3 && str.substring(0, 3).equals("defintely not")) {
 			return str;
 		}
-		return "not " + str;
+		return "defintely not " + str;
 	}
 }
